39e9ff932c7eb665fb526de8a3220ec280d1a572
techgeekster96677 <techgeekster96677@gmail.com>
2026-03-20 07:13:50 +0800

    Update main menu sliders

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -3,12 +3,17 @@ using UnityEngine.SceneManagement;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.Audio;
-
+using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
+
+    public Slider musicSlider;
+    public Slider sfxSlider;
+
     private void Start()
     {
+        LoadVolume();
         MusicManager.instance.PlayMusic("MainMenu");
     }
     public void StartGame()
@@ -47,5 +52,30 @@ public class MainMenu : MonoBehaviour
     public void SetSFXVolume(float volume)
     {
         audioMixer.SetFloat("SFXVolume", volume);
+    }
+
+    public void SaveVolume()
+    {
+        audioMixer.GetFloat("MusicVolume", out float musicVolume);
+        PlayerPrefs.SetFloat("MusicVolume", musicVolume);
+
+        audioMixer.GetFloat("SFXVolume", out float sfxVolume);  
+        PlayerPrefs.SetFloat("SFXVolume", sfxVolume);
+    }
+
+    public void LoadVolume()
+    {
+        if (PlayerPrefs.HasKey("MusicVolume"))
+        {
+            float musicVolume = PlayerPrefs.GetFloat("MusicVolume");
+            audioMixer.SetFloat("MusicVolume", musicVolume);
+            musicSlider.value = musicVolume;
+        }
+        if (PlayerPrefs.HasKey("SFXVolume"))
+        {
+            float sfxVolume = PlayerPrefs.GetFloat("SFXVolume");
+            audioMixer.SetFloat("SFXVolume", sfxVolume);
+            sfxSlider.value = sfxVolume;
+        }
     }
 }
